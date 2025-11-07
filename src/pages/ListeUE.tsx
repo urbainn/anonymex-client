219840fe7703cca86ca89efc61d0d83b7e3d86ca
0maxjlt <maximejouault34@gmail.com>
2025-11-08 00:37:35 +0100

    idee 1

---
+++ src/pages/ListeUE.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+export default function ListeUE() {
+
+    interface Epreuve {
+        id_session: number;
+        code_epreuve: string;
+        nom: string;
+        statut: string;
+        date_epreuve: Date;
+    }
+
+
+
+
+  return (
+    <>
+    </>
+  );
+}
