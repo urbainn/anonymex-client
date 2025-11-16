d07196935dbefcbbf514618149a6d386101c7d9a
urbainn <ubrainfr@gmail.com>
2025-11-16 19:51:10 +0100

    ajout contrats invitation/inscription

--- src/contracts/utilisateurs.ts
+++ src/contracts/utilisateurs.ts
@@ -19,11 +19,26 @@ export const LoginUtilisateurSchema = UtilisateurSchema.pick({ email: true }).ex
 
 export const UpdateUtilisateurSchema = UtilisateurSchema.omit({ id: true, idRole: true }).partial();
 
+export const GetAuthInfoSchema = z.object({ premiereConnexion: z.boolean() });
+
+export const PostInvitationSchema = z.object({ email: z.string(), jetonInvitation: z.string() });
+
+export const CreerUtilisateurSchema = z.object({
+    jetonInvitation: z.string(),
+    email: z.string(),
+    nom: z.string(),
+    prenom: z.string(),
+    motDePasse: z.string()
+});
+
 // --- Types ---
 export type APIUtilisateur = z.infer<typeof UtilisateurSchema>;
 export type APIListUtilisateur = z.infer<typeof ListUtilisateursSchema>;
 export type APILoginBody = z.infer<typeof LoginUtilisateurSchema>;
 export type APIUpdateUtilisateur = z.infer<typeof UpdateUtilisateurSchema>;
+export type APIGetAuthInfo = z.infer<typeof GetAuthInfoSchema>;
+export type APICreateUtilisateur = z.infer<typeof CreerUtilisateurSchema>;
+export type APIInvitationInfo = z.infer<typeof PostInvitationSchema>;
 
 // --- Endpoints API ---
 export const getUtilisateurs = () => {
@@ -31,7 +46,7 @@ export const getUtilisateurs = () => {
 }
 
 export const loginUtilisateur = (loginData: APILoginBody) => {
-    return apiRequest<APILoginBody, APIBoolResponse>('POST', '/utilisateurs/login', loginData, LoginUtilisateurSchema);
+    return apiRequest<APILoginBody, APIBoolResponse>('POST', '/utilisateurs/auth/login', loginData, LoginUtilisateurSchema);
 }
 
 export const updateUtilisateur = (id: number, updateData: APIUpdateUtilisateur) => {
@@ -40,4 +55,16 @@ export const updateUtilisateur = (id: number, updateData: APIUpdateUtilisateur) 
 
 export const deleteUtilisateur = (id: number) => {
     return apiRequest<null, APIBoolResponse>('DELETE', `/utilisateurs/${id}`);
+}
+
+export const getAuthInfo = () => {
+    return apiRequest<null, APIGetAuthInfo>('GET', '/utilisateurs/auth/info');
+}
+
+export const getInvitationInfo = (jetonInvitation: string, email: string) => {
+    return apiRequest<APIInvitationInfo, APIBoolResponse>('POST', '/utilisateurs/invitations/info', { jetonInvitation, email }, PostInvitationSchema);
+}
+
+export const creerUtilisateur = (createData: APICreateUtilisateur) => {
+    return apiRequest<APICreateUtilisateur, APIBoolResponse>('POST', '/utilisateurs', createData, CreerUtilisateurSchema);
 }
