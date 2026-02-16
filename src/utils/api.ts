f5f420aaec92acb36bd3359004ae343ca73d12d2
urbainn <ubrainfr@gmail.com>
2026-02-17 00:30:10 +0100

    téléversement fichier XLSX

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,6 @@
 import type z from "zod";
 
-const URL_API_BASE = "http://localhost:3000/api";
+export const URL_API_BASE = "http://localhost:3000/api";
 
 export async function apiRequest<B /* type body */, R /* type réponse */>(
     methode: 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH',
