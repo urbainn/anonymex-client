dcef1aeb3eae3c2b27d64f26d698106775eaa957
antoinedmns <antoine.damiens1@orange.fr>
2026-03-26 16:19:37 +0100

    ajout contract etudiants

---
+++ src/contracts/etudiants.ts
@@ -0,0 +1,27 @@
+import { z } from "zod";
+import { apiRequest } from "../utils/api";
+
+// --- Schémas ---
+export const EtudiantSchema = z.object({
+    numeroEtudiant: z.number().int().positive(),
+    nom: z.string(),
+    prenom: z.string()
+});
+
+export const NewEtudiantSchema = EtudiantSchema;
+
+export const UpdateEtudiantSchema = EtudiantSchema.pick({ nom: true, prenom: true }).partial();
+
+// --- Types ---
+export type APIEtudiant = z.infer<typeof EtudiantSchema>;
+export type APINewEtudiant = z.infer<typeof NewEtudiantSchema>;
+export type APIUpdateEtudiant = z.infer<typeof UpdateEtudiantSchema>;
+
+// --- Endpoint API ---
+export const createSession = (newEtudiant: APINewEtudiant) => {
+    return apiRequest<APINewEtudiant, APIEtudiant>('POST', '/etudiants/', newEtudiant, NewEtudiantSchema);
+}
+
+export const updateSession = (numero: number, updateData: APIUpdateEtudiant) => {
+    return apiRequest<APIUpdateEtudiant, APIEtudiant>('PATCH', `/etudiants/${numero}`, updateData, UpdateEtudiantSchema);
+}
