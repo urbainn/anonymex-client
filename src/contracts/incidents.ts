cb677311d9c7dcc43e1e2f87fc9bd1647757afbb
urbainn <ubrainfr@gmail.com>
2026-03-26 09:06:20 +0100

    feat(incidents): ajout routes

--- src/contracts/incidents.ts
+++ src/contracts/incidents.ts
@@ -1,4 +1,5 @@
 import { z } from "zod";
+import { apiRequest } from "../utils/api";
 
 // --- Schémas ---
 export const IncidentSchema = z.object({
@@ -12,10 +13,26 @@ export const IncidentSchema = z.object({
 });
 
 export const ListIncidentsSchema = z.object({
-    incidents: z.array(IncidentSchema.pick({ idIncident: true, titre: true, details: true}))
+    incidents: z.array(IncidentSchema.pick({ idIncident: true, titre: true, details: true }))
 });
+
+export const PartielIncidentSchema = IncidentSchema.pick({ idIncident: true, codeAnonymat: true, noteQuart: true });
 
 // --- Types ---
 export type APIIncident = z.infer<typeof IncidentSchema>;
 export type APIListIncidents = z.infer<typeof ListIncidentsSchema>;
+export type APIPartielIncident = z.infer<typeof PartielIncidentSchema>;
 
+export interface APIReponseCorrectionIncident { success: boolean, message?: string, incidents?: APIIncident[] }
+
+export const getIncident = (sessionId: number, epreuveCode: string, incidentId: number) => {
+    return apiRequest<null, APIIncident>('GET', `/sessions/${sessionId}/epreuves/${epreuveCode}/incidents/${incidentId}`);
+}
+
+export const getIncidents = (sessionId: number, epreuveCode: string) => {
+    return apiRequest<null, APIListIncidents>('GET', `/sessions/${sessionId}/epreuves/${epreuveCode}/incidents`);
+}
+
+export const corrigerIncident = (sessionId: number, epreuveCode: string, idIncident: number, codeAnonymat: string, noteQuart: number) => {
+    return apiRequest<{ codeAnonymat: string, noteQuart: number }, APIReponseCorrectionIncident>('POST', `/sessions/${sessionId}/epreuves/${epreuveCode}/incidents/${idIncident}`, { codeAnonymat, noteQuart });
+}
